9269b17ebb37b1e1aef5ee077046fd275c7696ec
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-03-16 17:17:11 +0300

    New role added

---
+++ src/Authorization/Roles/StayUserRole.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace NextDeveloper\Stay\Authorization\Roles;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use NextDeveloper\CRM\Database\Models\AccountManagers;
+use NextDeveloper\IAM\Authorization\Roles\AbstractRole;
+use NextDeveloper\IAM\Authorization\Roles\IAuthorizationRole;
+use NextDeveloper\IAM\Database\Models\Users;
+use NextDeveloper\IAM\Helpers\UserHelper;
+
+class StayUserRole extends AbstractRole implements IAuthorizationRole
+{
+    public const NAME = 'stay-user';
+
+    public const LEVEL = 50;
+
+    public const DESCRIPTION = 'Stay module user role. This role is required to access hotels, room types and rooms in general. Also to make subscriptions.';
+
+    public const DB_PREFIX = 'stay';
+
+    /**
+     * Applies basic member role sql for Eloquent
+     *
+     * @param Builder $builder
+     * @param Model $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        /**
+         * Here user will be able to list all models, because by default, sales manager can see everybody.
+         */
+
+    }
+
+    public function checkPrivileges(Users $users = null)
+    {
+        //return UserHelper::hasRole(self::NAME, $users);
+    }
+
+    public function getModule()
+    {
+        return 'stay';
+    }
+
+    public function allowedOperations() :array
+    {
+        return [
+            'stay_hotels:read',
+            'stay_rooms:read',
+            'stay_room_types:read',
+            'stay_reservations:read',
+            'stay_reservations:update',
+            'stay_reservations:create',
+            'stay_reservations:checkin',
+            'stay_reservations:checkout',
+            'stay_reservations:cancel',
+            'stay_reservations:refund',
+            'stay_reservations:confirm',
+            'stay_reservations:unconfirm',
+            'stay_reservations:assign',
+            'stay_reservations:unassign',
+            'stay_reservations:move',
+            'stay_reservations:unmove',
+            'stay_reservations:change',
+            'stay_reservations:unchange',
+            'stay_room_prices:read',
+        ];
+    }
+
+    public function getLevel(): int
+    {
+        return self::LEVEL;
+    }
+
+    public function getDescription(): string
+    {
+        return self::DESCRIPTION;
+    }
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function canBeApplied($column)
+    {
+        if(self::DB_PREFIX === '*') {
+            return true;
+        }
+
+        if(Str::startsWith($column, self::DB_PREFIX)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getDbPrefix()
+    {
+        return self::DB_PREFIX;
+    }
+}
